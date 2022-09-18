5597f55064e110bbfd8933c3e7b0a5e7e7108405
jpaquim <joaopaquim@gmail.com>
2022-09-18 16:27:21 +0100

    Build binaryen with zig cc/c++

--- build_binaryen.sh
+++ build_binaryen.sh
@@ -4,6 +4,10 @@ set -ex
 
 DIR=$PWD/binaryen
 JOBS=8
+
+export CC="$(which zig) cc"
+export CXX="$(which zig) c++"
+
 pushd $DIR
   cmake . -DBUILD_STATIC_LIB=on -DBUILD_TESTS=off -DBUILD_TOOLS=off
   make -j $JOBS
